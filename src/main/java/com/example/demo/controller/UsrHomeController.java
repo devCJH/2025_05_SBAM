c10ce6ea909083f9bd50041f06802acef76f31b5
devCJH <http-@naver.com>
2025-05-12 14:47:51 +0900

    jstl, el, model

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsrHomeController {
 	
 	@GetMapping("/usr/home/main")
-	public String showMain() {
+	public String showMain(Model model) {
+		
+		model.addAttribute("a", 789);
+		model.addAttribute("b", 456);
+		model.addAttribute("c", 123);
+		
 		return "usr/home/main";
 	}
 	
