e8bcbf5da3b09d1fb59c5093949b5f4fb8868296
devCJH <http-@naver.com>
2025-05-12 12:45:53 +0900

    jsp사용

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,15 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	
 	@GetMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "게시판 메인페이지 입니다.";
+		return "usr/home/main";
 	}
 	
 }
