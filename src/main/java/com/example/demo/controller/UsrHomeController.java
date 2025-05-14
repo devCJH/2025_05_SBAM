a6c171b94a13e67122410e9faac19fb9bfe8bc74
devCJH <http-@naver.com>
2025-05-14 10:39:09 +0900

    form, iuput, submit 예제

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
@@ -14,6 +16,17 @@ public class UsrHomeController {
 	@GetMapping("/")
 	public String showRoot() {
 		return "redirect:/usr/home/main";
+	}
+	
+	@GetMapping("/usr/home/test")
+	public String inputTest() {
+		return "usr/home/test";
+	}
+	
+	@PostMapping("/usr/home/test1")
+	@ResponseBody
+	public String inputTest1(String title, String content) {
+		return title + "/" + content;
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/test.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div>input 테스트 페이지</div>
+	
+	<form action="test1" method="post">
+		<div>input 입력 : <input name="title" type="text" /></div>
+		<div>textarea 입력 : <textarea name="content"></textarea></div>
+		
+		<button>저장버튼1</button>
+	</form>
+</body>
+</html>
